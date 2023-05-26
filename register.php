c62af67f902b75436b3731f5e16c79191628ca3b
siddhartha64 <128678545+siddhartha64@users.noreply.github.com>
2023-05-27 00:09:18 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,50 +1,48 @@
 
+
+
 <?php
 
-// Check if user is already logged in, redirect to another page
 
-
-
-// Database connection parameters
 $servername = "localhost";
 $username = "siddhu";
 $password = "amma";
 $dbname = "userinfo";
 
-// Handle registration form submission
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
    
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
         echo '111';die("Connection failed: " . $conn->connect_error);
     }
-    // Validate form data
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email   =  $_POST['email'];
+   
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+    $email   =  htmlspecialchars($_POST['email']);
     
     
-       // Check if the username already exists in the database
-    $stmt = $conn->prepare("SELECT * FROM USERS WHERE Username = '$username'");
+      
+    $stmt = $conn->prepare("SELECT * FROM USERS WHERE Username = ?");
+    $stmt ->bind_param("s",$username);
+    
     $stmt->execute();
+
     $result = $stmt->get_result();
    
     if ($result->num_rows > 0) {
-     //   $stmt->close();
-       // $conn->close();
+     
         echo "Username already exists. Please choose a different username.";
         exit();
     }
-   // $stmt->close();
-
-    // Insert the new user into the database
+   
    else{
     
     $reg = $conn->prepare("INSERT INTO USERS(Username, Password,Email) VALUES (?,?,?)");
 
     $reg->bind_param("sss",$username, $password,$email);
     $reg->execute();
-    echo"11";
+   
     if($reg->execute()==True){
     header("Location: login.php");
     exit();}
