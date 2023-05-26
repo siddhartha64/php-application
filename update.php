1967a432324da0c0c092290844bbf8a0624810a9
siddhartha64 <128678545+siddhartha64@users.noreply.github.com>
2023-05-27 00:10:17 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,49 +1,60 @@
 <?php
 session_start();
-$servername = "localhost";
-$Username = "siddhu";
-$Password = "amma";
-$dbname = "userinfo";
-$conn = mysqli_connect($servername, $Username, $Password, $dbname);
+if($_SESSION['user_name']!=NULL){
+    $servername = "localhost";
+    $Username = "siddhu";
+    $Password = "amma";
+    $dbname = "userinfo";
+    $conn = mysqli_connect($servername, $Username, $Password, $dbname);
 
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-
-if (isset($_POST['submit'])) {
-    
-    $username = $_POST['Username'];
-    $password = $_POST['Password'];
-    $email = $_POST['Email'];
-    echo"a";
-    
-    $sql = "UPDATE USERS SET  Password='$password', Email='$email' WHERE Username='$username' ";
-    echo"b";
-    if (mysqli_query($conn, $sql)) {
-        echo "User record updated successfully";
-    } else {
-        echo "Error updating user record: " . mysqli_error($conn);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
-}
+    $stmt = $conn->prepare("SELECT Admin FROM USERS WHERE Username = ?");
+    $stmt->bind_param("s", $_SESSION['user_name']);
+    $stmt->execute();
+    $stmt->bind_result($admin);
+    $stmt->fetch();
+    $stmt->close();
+
+    if ($admin !== null) {
+
+    if (isset($_POST['submit'])) {
+        
+        $username = htmlspecialchars($_POST['Username']);
+        $password = htmlspecialchars($_POST['Password']);
+        $email = htmlspecialchars($_POST['Email']);
+        echo"a";
+        
+        $sql =$conn->prepare( "UPDATE USERS SET  Password= ?, Email= ? WHERE Username= ? ");
+        $sql->bind_param("sss", $password,$email,$username);
+        $sql->execute();
+        echo"b";
+
+        if  ($sql->execute()==True) {
+            echo "User record updated successfully";
+        } else {
+            echo "Error updating user record: " . mysqli_error($conn);
+        }
+    }
 
 
 
-$sql = ("SELECT * FROM USERS");
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result);
+    $sql = ("SELECT * FROM USERS");
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
 
 
 
-echo "<form method='post'>";
-echo "<input type='hidden' name='id' value='" . $row['Id'] . "'>";
-echo "Username: <input type='text' name='Username' value='" . $row['Username'] . "'><br>";
-echo "Password: <input type='password' name='Password' value='" . $row['Password'] . "'><br>";
-echo "Email: <input type='text' name='Email' value='" . $row['Email'] . "'><br>";
-echo "<input type='submit' name='submit' value='Update'>";
-echo "</form>";
+    echo "<form method='post'>";
+    echo "<input type='hidden' name='id' value='" . $row['Id'] . "'>";
+    echo "Username: <input type='text' name='Username' value='" . $row['Username'] . "'><br>";
+    echo "Password: <input type='password' name='Password' value='" . $row['Password'] . "'><br>";
+    echo "Email: <input type='text' name='Email' value='" . $row['Email'] . "'><br>";
+    echo "<input type='submit' name='submit' value='Update'>";
+    echo "</form>";
 
 
-mysqli_close($conn);
-?>
+    mysqli_close($conn);}}
+    ?>
